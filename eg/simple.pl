0d89a37f123671d131dca68d5de25072917ededc
Kenta, Kobayashi <kentafly88@gmail.com>
2018-05-03 09:49:07 +0900

    add relayer example

--- eg/simple.pl
+++ eg/simple.pl
@@ -2,47 +2,13 @@ use strict;
 use warnings;
 use utf8;
 
+use Test::More;
 use SQL::Relationship;
 use SQL::Maker;
 use DBI;
 
 my $builder = SQL::Maker->new( driver => 'SQLite' );
 my $dbh = DBI->connect('dbi:SQLite:dbname=test.db');
-
-$dbh->do('DROP TABLE module');
-$dbh->do('DROP TABLE author');
-$dbh->do( '
-CREATE TABLE module (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  author_id INTEGER,
-  FOREIGN KEY (author_id) REFERENCES author(id)
-)' );
-
-$dbh->do( '
-CREATE TABLE author (
-  id INTEGER PRIMARY KEY NOT NULL,
-  name VARCHAR(255)
-);
-' );
-
-$dbh->do(q!INSERT INTO author (id, name) VALUES (1, 'songmu')!);
-$dbh->do(q!INSERT INTO author (id, name) VALUES (2, 'karupanerura')!);
-
-$dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (1, 'DBIx::Schema::DSL', 1)!);
-$dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (2, 'Riji', 1)!);
-$dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (3, 'Aniki', 2)!);
-$dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (4, 'Time::Strptime', 2)!);
-$dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (5, 'TOML', 2)!);
-
-my $relationship = SQL::Relationship->new(
-    src_table    => 'module',
-    src_columns  => [qw/author_id/],
-    dest_table   => 'author',
-    dest_columns => [qw/id/],
-    has_many     => 0,
-    fetcher      => \&fetcher,
-);
 
 sub fetcher {
     my ( $relationship, $dest_table, $dest_columns, $where, $opt ) = @_;
@@ -51,10 +17,69 @@ sub fetcher {
     $dbh->selectall_arrayref( $sql, { Slice => {} }, @binds );
 }
 
-use Test::More;
+sub relayer {
+    my ( $relationship, $src_rows, $dest_rows ) = @_;
+
+    my %dest_map = map { $relationship->value_key_from_dest($_) => $_ } @$dest_rows;
+
+    for my $src_row (@$src_rows) {
+        my $dest_row = $dest_map{ $relationship->value_key_from_src($src_row) };
+        $src_row->{ $relationship->name } = $dest_row;
+    }
+}
+
+my $relationship = SQL::Relationship->new(
+    src_table    => 'module',
+    src_columns  => [qw/author_id/],
+    dest_table   => 'author',
+    dest_columns => [qw/id/],
+    has_many     => 0,
+    fetcher      => \&fetcher,
+    relayer      => \&relayer,
+);
+
+setup($dbh);
+
 my $src_rows = $relationship->fetch_src_rows( { name => [ 'Aniki', 'TOML' ] }, { columns => ['name'] } );
 my $dest_rows = $relationship->fetch_dest_rows( $src_rows, {}, { columns => ['name'] } );
 is_deeply $src_rows, [ { author_id => 2, name => 'Aniki' }, { author_id => 2, name => 'TOML' } ];
 is_deeply $dest_rows, [ { id => 2, name => 'karupanerura' } ];
 
+$relationship->relate_dest_rows( $src_rows, $dest_rows );
+is_deeply $src_rows,
+    [
+    { author_id => 2, name => 'Aniki', author => { id => 2, name => 'karupanerura' } },
+    { author_id => 2, name => 'TOML',  author => { id => 2, name => 'karupanerura' } }
+    ];
+
 done_testing;
+
+sub setup {
+    my $dbh = shift;
+
+    $dbh->do('DROP TABLE module');
+    $dbh->do('DROP TABLE author');
+    $dbh->do( '
+    CREATE TABLE module (
+      id INTEGER PRIMARY KEY NOT NULL,
+      name VARCHAR(255),
+      author_id INTEGER,
+      FOREIGN KEY (author_id) REFERENCES author(id)
+    )' );
+
+    $dbh->do( '
+    CREATE TABLE author (
+      id INTEGER PRIMARY KEY NOT NULL,
+      name VARCHAR(255)
+    );
+    ' );
+
+    $dbh->do(q!INSERT INTO author (id, name) VALUES (1, 'songmu')!);
+    $dbh->do(q!INSERT INTO author (id, name) VALUES (2, 'karupanerura')!);
+
+    $dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (1, 'DBIx::Schema::DSL', 1)!);
+    $dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (2, 'Riji', 1)!);
+    $dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (3, 'Aniki', 2)!);
+    $dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (4, 'Time::Strptime', 2)!);
+    $dbh->do(q!INSERT INTO module (id, name, author_id) VALUES (5, 'TOML', 2)!);
+}
